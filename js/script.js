db45a9dbe1e1bdcfc80e824585fe788339031863
MCStuart <47284075+MCStuart@users.noreply.github.com>
2021-04-28 21:37:03 -0700

    create function to build sibling child rectangle elements

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
-let divBuilder = (indexId) => {
+let divBuilder = (i) => {
   let square = document.createElement('div');
 
   square.className = 'square';
 
-  square.id = indexId;
+  square.id = i;
 
   document.getElementById('squares').appendChild(square);
 }
 
-// let nodes = [...Array(60)];
+let childDivBuilder = (i) => {
+  let rectangle1 = document.createElement('div');
+  let rectangle2 = document.createElement('div');
 
-// for(const index of nodes.keys()) {
-//   divBuilder(Number(index) + 1)
+  rectangle1.className = 'rectangle';
+  rectangle1.id = 1;
+
+  rectangle2.className = 'rectangle';
+  rectangle2.id = 2;
+
+  document.getElementById(i + 1).appendChild(rectangle1);
+  document.getElementById(i + 1).appendChild(rectangle2);
+}
 
 for(i=0;i<60;i++) {
   divBuilder(i+1)
+  childDivBuilder(i)
 }
+
