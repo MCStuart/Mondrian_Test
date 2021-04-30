21b3323b2d16fc0a13584b435f7287dbadd856e0
MCStuart <47284075+MCStuart@users.noreply.github.com>
2021-04-29 18:51:21 -0700

    remove unnecessary switch statement

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let divBuilder = (i) => {
   let square = document.createElement('div');
 
-  square.className = `square colorCombination${Math.floor(Math.random()*10)}`;
+  square.className = `square colorCombination${Math.floor(Math.random() * (7 - 1 + 1)) + 1}`;
 
   square.id = `s${i}`;
 
@@ -17,25 +17,21 @@ let childDivBuilder = (i) => {
 
   rectangle2.className = 'rectangle';
   rectangle2.id = 'second';
-  
+
   document.getElementById(`s${i + 1}`).appendChild(rectangle1);
   document.getElementById(`s${i + 1}`).appendChild(rectangle2);
 }
 
-for(i=0;i<60;i++) {
-  divBuilder(i+1)
+for (i = 0; i < 60; i++) {
+  divBuilder(i + 1)
   childDivBuilder(i)
 }
 
-document.addEventListener('click', function ( e ) {
-  
+document.addEventListener('click', function (e) {
   let elementId = e.target.parentNode.id
-  console.log(e.target.parentNode.id);
-
   let currentColorCombo = e.target.parentNode.classList[1];
-  console.log(currentColorCombo)
 
-  document.querySelector(`#${elementId}`).classList.replace(currentColorCombo, `colorCombination${Math.floor(Math.random()*10)}`)
+  document.querySelector(`#${elementId}`).classList.replace(currentColorCombo, `colorCombination${Math.floor(Math.random() * (7 - 1 + 1)) + 1}`)
 });
 
 
